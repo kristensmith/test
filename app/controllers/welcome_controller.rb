ca7fcbc5046421cd50aea032eab6e444c64978a1
Mark Simcoe <msimcoe@comcast.net>
2014-08-06 12:08:45 -0400

    second comment

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,28 @@
 class WelcomeController < ApplicationController
+ 
+def types
+  end
+
   def index
+   	@name = "Mark Simcoe"
+  	@age = 57
+  	@array = [34,45,99]
+  	@sum = add_numbers(@array[0],@array[1],@array[2])
+  	@diff = sub_numbers(@array[0],@array[1],@array[2])
   end
 
   def about
+  	@name = "Mark Simcoe"
+  	@age = 57
+  	@array = [34,45,99]
+  	@sum = add_numbers(@array[0],@array[1],@array[2])
+  	@diff = sub_numbers(@array[0],@array[1],@array[2])
   end
-end
+  def add_numbers(x,y,z)
+  	x + y + z
+  end
+  def sub_numbers(x,y,z)
+  	x - y - z
+  end
+end  
+
